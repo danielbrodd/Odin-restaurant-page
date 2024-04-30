21f28c9e58a8512a8d7288fd70f9a80d49c2ace0
danielbrodd <77890040+danielbrodd@users.noreply.github.com>
2024-04-30 13:20:07 +0000

    finish menu module, add more styling apropriate to menu model, add asset menu.png

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,11 +1,23 @@
 import clearContent from "./clearContent";
+import menuImage from '../assets/menu.png'
 
 function loadMenuPage () {
     const content = document.querySelector('#content');
-
     clearContent(content);
 
-    
+
+    let menu = document.createElement('img');
+    let outerMenu = document.createElement('div');
+    outerMenu.classList.add('menuBorder');
+    menu.src =  menuImage;
+    menu.classList.add('menu');
+    outerMenu.appendChild(menu);
+    content.appendChild(outerMenu);
+
+    let credit = document.createElement('em');
+    credit.innerHTML= "menu design by:@aelinadesign [https://www.canva.com/p/aelinadesign/]"
+    content.appendChild(credit);
+
 }
 
 export default loadMenuPage;
